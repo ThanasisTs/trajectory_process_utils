c15777b7b3aa691bb5577ce6518cc40bfe890706
Thanasists <thtsitos@gmail.com>
2020-06-03 19:53:41 +0300

    changed smoothing service

--- trajectory_process_utils/scripts/trajectory_smoothing_server.py
+++ trajectory_process_utils/scripts/trajectory_smoothing_server.py
@@ -12,25 +12,27 @@ def handle_smoothing(req):
 	z = req.z
 
 	x_1,y_1,z_1 = trajectory_smoothing_function.main(x,y,z)
+	x_smooth_all, y_smooth_all, z_smooth_all = x_1, y_1, z_1
 	x_smooth,y_smooth,z_smooth = [x_1[0]],[y_1[0]],[z_1[0]]
 
-	print (len(x_1))
+	# print (len(x_1))
 	a = (x_1[0],y_1[0],z_1[0])
-	for i in range(len(x_1)):
+	for i in range(1, len(x_1)):
 		b = (x_1[i],y_1[i],z_1[i])
 		dst = distance.euclidean(a, b)
 		
-		if dst > 0.0055:
+		if dst > 0.012:
 			x_smooth.append(x_1[i])
 			y_smooth.append(y_1[i])
 			z_smooth.append(z_1[i])
 			a = (x_1[i],y_1[i],z_1[i])
 
 	rospy.loginfo("Wait to plot the trajectory")
+	# x_smooth, y_smooth, z_smooth = x_1, y_1, z_1
 	# functions.print2D([x,y,z],[x_smooth,y_smooth,z_smooth])
 	# functions.print3D([x,y,z],[x_smooth,y_smooth,z_smooth])
-	print (len(x_smooth))
-	return SmoothingResponse(x_smooth,y_smooth,z_smooth)
+
+	return SmoothingResponse(x_smooth,y_smooth, z_smooth, x_smooth_all, y_smooth_all, z_smooth_all)
 
 
 def trajectory_smoothing_server():
